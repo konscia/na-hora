dc24468c4f6638316545c12a30397b76a7988ca8
Kaleu Caminha <kaleu.caminha@gmail.com>
2012-06-19 14:40:41 -0300

    atualizacao do arquivo da base de dados

--- na_hora.sql
+++ na_hora.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: Jun 13, 2012 as 02:36 PM
+-- Tempo de Geração: Jun 19, 2012 as 02:39 PM
 -- Versão do Servidor: 5.5.17
 -- Versão do PHP: 5.3.6
 
@@ -74,6 +74,7 @@ CREATE TABLE IF NOT EXISTS `novidades` (
   `resumo` varchar(256) NOT NULL,
   `data` date NOT NULL,
   `link` varchar(200) DEFAULT NULL,
+  `categoria` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
@@ -81,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `novidades` (
 -- Extraindo dados da tabela `novidades`
 --
 
-INSERT INTO `novidades` (`id`, `titulo`, `texto`, `resumo`, `data`, `link`) VALUES
-(1, 'Material Prova Amanhã', 'Olá pessoal. Segue em anexo o material de prova que devem estudar para amanhã.\r\n\r\nOs tópicos principais são:\r\n\r\n* Tópico 1\r\n* Tópico 2\r\n* Tópico 3\r\n\r\nBoa sorte e..até breve.', 'Vejam aqui o material e tópicos que devem ser estudados para a prova de amanhã', '2012-06-05', 'www.caelum.com.br'),
-(2, 'Avaliação disciplina', 'Olá pessoal, gostaria que me ajudassem a melhorar a disciplina para os próximos semestres.\r\n\r\nDeixe nos comentários sua avaliação sobre o nosso trabalho.', 'ajude a melhorar a disciplina com a sua avaliação', '2012-06-05', NULL),
-(3, 'Título da novidade', 'null', 'esta novidade blá blá', '2012-06-05', 'http://www.google.com.br');
+INSERT INTO `novidades` (`id`, `titulo`, `texto`, `resumo`, `data`, `link`, `categoria`) VALUES
+(1, 'Material Prova Amanhã', 'Olá pessoal. Segue em anexo o material de prova que devem estudar para amanhã.\r\n\r\nOs tópicos principais são:\r\n\r\n* Tópico 1\r\n* Tópico 2\r\n* Tópico 3\r\n\r\nBoa sorte e..até breve.', 'Vejam aqui o material e tópicos que devem ser estudados para a prova de amanhã', '2012-06-05', 'www.caelum.com.br', 1),
+(2, 'Avaliação disciplina', 'Olá pessoal, gostaria que me ajudassem a melhorar a disciplina para os próximos semestres.\r\n\r\nDeixe nos comentários sua avaliação sobre o nosso trabalho.', 'ajude a melhorar a disciplina com a sua avaliação', '2012-06-05', NULL, 2),
+(3, 'Título da novidade', 'null', 'esta novidade blá blá', '2012-06-05', 'http://www.google.com.br', 1);
